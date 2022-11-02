f56805234ece502b031ae52739ea3aaa12313608
handsomevictor <857730556@qq.com>
2022-11-02 16:48:33 +0100

    update victor

--- main.py
+++ main.py
@@ -67,6 +67,67 @@ def combine_concurrent(crypto_list, fiat_list, days, save_dir=None, max_workers=
     print(f'Total time: {end_time - start_time:.2f} seconds')
 
 
+def days_list(start_date=None, end_date=None, N=10):
+    """
+    :param start_date: in datetime format
+    :param end_date:
+    :param N:
+    :return:
+    """
+    if start_date and end_date:
+        date_list = [(end_date - start_date).days + 1 for i in range(len(cryptos))]
+    else:
+        days_list = [N for i in range(len(cryptos))]
+
+    return date_list
+
+
+def read_single_result_file(id, fiat, days):
+    df = pd.read_csv(os.path.join(os.getcwd(), 'data_download', f'{id}_{fiat}_{days}.csv'),
+                     index_col=0).reset_index()
+
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df['hour'] = df['timestamp'].dt.hour
+
+    df['timestamp'] = df['timestamp'].dt.strftime('%Y-%m-%d')
+
+    return df[df['hour'] == 0][['timestamp', 'market_cap', 'supply', 'price']].set_index('timestamp')
+
+
+def combine_several_id(id_list, days_list, fiat_list):
+    file_dir = os.path.join(os.getcwd(), 'data_download')
+    target_zip = zip(id_list, days_list, fiat_list)
+
+    res_df = pd.DataFrame()
+
+    for id, day, fiat in target_zip:
+        df = read_single_result_file(id, fiat, day)
+        df['id'] = id
+        res_df = pd.concat([res_df, df], axis=0)
+
+    return res_df[['id', 'supply']]
+
+
+def reformat_combined_result(id_list, days_list, fiat_list):
+    df = combine_several_id(id_list, days_list, fiat_list)
+    all_timestamp = sorted(df.index.unique().tolist())
+    res = pd.DataFrame(index=all_timestamp)
+
+    temp = {}
+    for id in id_list:
+        temp[id] = df[df['id'] == id]['supply'].rename(id)
+
+    for id in id_list:
+        res = pd.concat([res, temp[id]], axis=1)
+    res = res.fillna(-1)
+
+    sub_dir = os.path.join(os.getcwd(), 'data_download', 'combined')
+    if not os.path.exists(sub_dir):
+        os.makedirs(sub_dir)
+    res.to_csv(os.path.join(sub_dir, 'combined_result.csv'))
+    return res
+
+
 if __name__ == '__main__':
     csv = pd.read_csv(os.path.join(os.getcwd(), 'ticker_cg.csv'))
 
@@ -92,11 +153,12 @@ if __name__ == '__main__':
     # start downloading
     cryptos = res
     fiats = ['usd' for i in range(len(cryptos))]
-    days_list = [10000 for i in range(len(cryptos))]
 
-    save_dir = os.path.join(os.getcwd(), 'data_download')
-    if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
+    # ---------
+    # for reformating and combining all results
+    id_list = ['bitcoin', 'ethereum-wormhole']
+    days_list = [3, 99]
+    fiat_list = ['usd', 'usd']
 
-    # combine_concurrent(crypto_list=cryptos, fiat_list=fiats, days=10000, max_workers=10, save_dir=save_dir)
-    combine(cryptos, fiats, days_list, save_dir=save_dir)
+    print(reformat_combined_result(id_list, days_list, fiat_list))
+
